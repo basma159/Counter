bc56d57b8d460dc07b59f660fa792e6d38e3d45e
Basma <bsmtbdalawl@gmail.com>
2026-04-05 23:52:26 +0200

    test

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ var pulse = document.getElementById("pulse")
 var num = document.getElementById("number")
 var minus = document.getElementById("minus")
 var reset = document.getElementById("reset")
+var click =document.createElement("audio")
 var counter = 0
 num.innerText = counter
 
 pulse.onclick = function () {
     counter++
     num.innerText = (counter)
+    click.play()
 }
 
 minus.onclick = function () {
